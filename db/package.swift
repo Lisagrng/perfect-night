9afa14293786710678ad41f1266d2529583827ba
Lisa Grange <lisa.grange42@gmail.com>
2023-02-10 15:49:24 +0100

    Et on pagaie

--- db/package.swift
+++ db/package.swift
@@ -1,3 +1,3 @@
 dependencies: [
-
-    .package(url: "https://github.com/fummicc1/SimpleRoulette.git", branch: "main")]
+    .package(url: "https://github.com/fummicc1/SimpleRoulette.git", branch: "main")
+    ]
