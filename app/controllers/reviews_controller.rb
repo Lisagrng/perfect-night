7090c13650403c211a8e9b2cda3e1e1dcd8d549b
Lisa Grange <lisa.grange42@gmail.com>
2023-02-17 12:24:30 +0100

    add reviews and débuggué les buggs

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,33 @@
+class ReviewsController < ApplicationController
+  before_action :set_actvity, only: %i[new create]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.activity = @activity
+    if @review.save
+      redirect_to activities_path(@activity)
+    else
+      render "activities/show", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to activity_path(@review.activity), status: :see_other
+  end
+
+  private
+
+  def set_actvity
+    @activity = Activity.find(params[:activity_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
