454217229f476ec0d1a40c050cbc70682ea285fe
Lisa Grange <lisa.grange42@gmail.com>
2023-02-24 12:24:48 +0100

    program dans la seedé

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_14_191035) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_24_105130) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_191035) do
     t.bigint "user_id"
     t.string "ville"
     t.string "categorie"
+    t.text "program"
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
