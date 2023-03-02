2067ebf074313670e4317362e115fa1812a7b754
Lisa Grange <lisa.grange42@gmail.com>
2023-03-02 10:15:03 +0100

    add new arrow pages

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :booking
-  # belongs_to :activity, :through => :booking
   validates :content, presence: true
-  validates :rating, inclusion: { in: [1, 2, 3, 4, 5], allow_nil: false }
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5], allow_nil: false }, presence: true
+  validates_length_of :content, maximum: 200
+  validates_length_of :content, minimum: 10
 end
